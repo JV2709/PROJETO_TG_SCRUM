175cef315a50472825789005eb00c26beebf84c4
JV2709 <136920296+JV2709@users.noreply.github.com>
2025-09-27 23:23:50 -0300

    Delete proj_1 directory

--- proj_1/proj_1_0/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Proj10Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'proj_1_0'
--- proj_1/proj_1_0/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),                                
-    path('api/stocks/', views.get_stock_data, name='stock_api'),       
-    path('dicionario-do-investidor/', views.DICIONARIO_view, name='dicionario'), 
-     path('investimentos/', views.investimentos_view, name='investimentos'),
-]
